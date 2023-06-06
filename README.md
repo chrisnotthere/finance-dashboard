e26ac7247bc64a9de4246055fcf21d3be06480e9
chris <mr.chrisnash@gmail.com>
2023-06-05 20:12:52 -0700

    dashboard layout

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # finance-dashboard
+
+This was created from a tutorial
+
+Tech used:
+Typescript, React, Redux toolkit, 
+Node, Express, MongoDB, 
+MUI, Recharts, Vite, 
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "@/scenes/navbar/index";
+import Dashboard from "@/scenes/dashboard/index";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -15,7 +16,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>  
-              <Route path="/" element={<div>Dashboard Page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>Predictions Page</div>} />
             </Routes>
           </Box>
